b4809a7b5de1358de380a3235f6d857efef0f596
Zander <zander@zanderf.net>
2021-08-19 23:03:44 -0500

    Rustfmt (I think)

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,16 @@ use rpc::RequestId;
 use smol::lock::MutexGuard;
 
 use {
-    rpc::{RpcMessage, RpcError},
+    rpc::{RpcError, RpcMessage},
+    smol::{
+        channel::{self, Receiver, Sender},
+        future::FutureExt,
+        io::{self, AsyncBufReadExt},
+        lock::Mutex,
+        stream::StreamExt,
+        Timer, Unblock,
+    },
     std::{collections::HashMap, sync::Arc, time::Duration},
-    smol::{Timer, Unblock, future::FutureExt, lock::Mutex, stream::StreamExt, channel::{self, Sender, Receiver}, io::{self, AsyncBufReadExt}}
 };
 
 pub use serde_json;
@@ -15,9 +22,9 @@ use serde_json::Value;
 pub mod rpc;
 
 pub struct Omegga {
-    channels_ref: Arc<Mutex<HashMap<rpc::RequestId, Sender<Result<Value, RpcError>>>>>,
+    channels_ref: Arc<Mutex<HashMap<rpc::RequestId, Sender<Result<Value, Option<RpcError>>>>>>,
 
-    last_id: i32
+    last_id: i32,
 }
 
 pub struct OmeggaWrapper {
@@ -27,7 +34,7 @@ pub struct OmeggaWrapper {
     pub stream: Arc<Receiver<RpcMessage>>,
 
     // internal stream parts
-    int_stream_sender: Arc<Sender<RpcMessage>>
+    int_stream_sender: Arc<Sender<RpcMessage>>,
 }
 
 impl OmeggaWrapper {
@@ -37,7 +44,8 @@ impl OmeggaWrapper {
 
         OmeggaWrapper {
             omegga: Arc::new(Mutex::new(Omegga::new())),
-            stream: Arc::new(stream_receiver), int_stream_sender: Arc::new(int_stream_sender)
+            stream: Arc::new(stream_receiver),
+            int_stream_sender: Arc::new(int_stream_sender),
         }
     }
 
@@ -60,7 +68,7 @@ impl OmeggaWrapper {
                 // parse into an RpcMessage
                 let message: RpcMessage = match serde_json::from_str(string.as_str()) {
                     Ok(m) => m,
-                    Err(_) => continue
+                    Err(_) => continue,
                 };
 
                 // match the message and let the handlers know
@@ -70,26 +78,30 @@ impl OmeggaWrapper {
                 let channels = self_locked.channels_ref.clone();
                 smol::spawn(async move {
                     match message {
-                        RpcMessage::Response { id, result, error, .. } => {
+                        RpcMessage::Response {
+                            id, result, error, ..
+                        } => {
                             let channels = &mut channels.lock().await;
                             match channels.get(&id) {
                                 Some(sender) => {
-                                    let res = match result {
-                                        Some(value) => Ok(value),
-                                        None => Err(error.expect("An error object from RPC was expected"))
-                                    };
+                                    let res =
+                                        match result {
+                                            Some(value) => Ok(value),
+                                            None => Err(error),
+                                        };
 
                                     // at this point, send the result over the sender
                                     // we can also remove this entry from the hashtable
                                     sender.send(res).await.unwrap();
                                     channels.remove(&id);
-                                },
-                                None => ()
+                                }
+                                None => (),
                             }
-                        },
-                        _ => sender_locked.send(message).await.unwrap()
+                        }
+                        _ => sender_locked.send(message).await.unwrap(),
                     }
-                }).detach();
+                })
+                .detach();
             }
         });
     }
@@ -97,36 +109,53 @@ impl OmeggaWrapper {
 
 impl Omegga {
     pub fn new() -> Self {
-        Omegga { channels_ref: Arc::new(Mutex::new(HashMap::new())), last_id: 0 }
+        Omegga {
+            channels_ref: Arc::new(Mutex::new(HashMap::new())),
+            last_id: 0,
+        }
     }
 
     /// Notify Omegga, given a method and some parameter.
     pub fn notify(method: &str, params: Value) {
-        println!("{}", serde_json::to_string(&RpcMessage::notification(method.into(), Some(params))).unwrap());
+        println!(
+            "{}",
+            serde_json::to_string(&RpcMessage::notification(method.into(), Some(params))).unwrap()
+        );
     }
 
     /// Notify Omegga with no parameter.
     pub fn tell(method: &str) {
-        println!("{}", serde_json::to_string(&RpcMessage::notification(method.into(), None)).unwrap());
+        println!(
+            "{}",
+            serde_json::to_string(&RpcMessage::notification(method.into(), None)).unwrap()
+        );
     }
 
     pub fn respond(id: RequestId, body: Result<Value, RpcError>) {
         let response = match body {
             Ok(value) => RpcMessage::response(id, Some(value), None),
-            Err(err) => RpcMessage::response(id, None, Some(err))
+            Err(err) => RpcMessage::response(id, None, Some(err)),
         };
 
         println!("{}", serde_json::to_string(&response).unwrap());
     }
 
-    pub async fn request(mutex: Arc<Mutex<Self>>, method: &str, params: Option<Value>) -> ResponseAwaiter {
+    pub async fn request(
+        mutex: Arc<Mutex<Self>>,
+        method: &str,
+        params: Option<Value>,
+    ) -> ResponseAwaiter {
         let local_self = &mut mutex.lock().await;
 
         local_self.last_id -= 1;
-        let request = RpcMessage::request(rpc::RequestId::Int(local_self.last_id), String::from(method), params);
+        let request = RpcMessage::request(
+            local_self.last_id.into(),
+            String::from(method),
+            params,
+        );
         println!("{}", serde_json::to_string(&request).unwrap());
 
-        let (rx, tx) = channel::bounded::<Result<Value, RpcError>>(1);
+        let (rx, tx) = channel::bounded::<Result<Value, Option<RpcError>>>(1);
         let channels = Arc::clone(&local_self.channels_ref);
 
         {
@@ -143,23 +172,24 @@ pub enum ResponseReceiveError {
     #[error("channel receive error")]
     Recv(#[from] smol::channel::RecvError),
     #[error("timed out")]
-    Timeout
+    Timeout,
 }
 
 pub struct ResponseAwaiter {
-    receiver: Receiver<Result<Value, RpcError>>
+    receiver: Receiver<Result<Value, Option<RpcError>>>,
 }
 
 impl ResponseAwaiter {
-    pub fn new(receiver: Receiver<Result<Value, RpcError>>) -> Self {
+    pub fn new(receiver: Receiver<Result<Value, Option<RpcError>>>) -> Self {
         ResponseAwaiter { receiver }
     }
 
-    pub async fn receive(self) -> Result<Result<Value, RpcError>, ResponseReceiveError> {
+    pub async fn receive(self) -> Result<Result<Value, Option<RpcError>>, ResponseReceiveError> {
         async move { Ok(self.receiver.recv().await?) }
             .or(async {
                 Timer::after(Duration::from_secs(15)).await;
                 Err(ResponseReceiveError::Timeout)
-            }).await
+            })
+            .await
     }
 }
--- src/rpc.rs
+++ src/rpc.rs
@@ -5,34 +5,74 @@ use serde_json::Value;
 pub struct RpcError {
     code: i32,
     message: String,
-    data: Option<Value>
+    data: Option<Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
 #[serde(untagged)]
 pub enum RequestId {
     Str(String),
-    Int(i32)
+    Int(i32),
+}
+
+impl From<String> for RequestId {
+    fn from(str: String) -> Self {
+        RequestId::Str(str)
+    }
+}
+
+impl From<i32> for RequestId {
+    fn from(i: i32) -> Self {
+        RequestId::Int(i)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(untagged)]
 pub enum RpcMessage {
-    Request { jsonrpc: String, id: RequestId, method: String, params: Option<Value> },
-    Response { jsonrpc: String, id: RequestId, result: Option<Value>, error: Option<RpcError> },
-    Notification { jsonrpc: String, method: String, params: Option<Value> }
+    Request {
+        jsonrpc: String,
+        id: RequestId,
+        method: String,
+        params: Option<Value>,
+    },
+    Response {
+        jsonrpc: String,
+        id: RequestId,
+        result: Option<Value>,
+        error: Option<RpcError>,
+    },
+    Notification {
+        jsonrpc: String,
+        method: String,
+        params: Option<Value>,
+    },
 }
 
 impl RpcMessage {
     pub fn request(id: RequestId, method: String, params: Option<Value>) -> RpcMessage {
-        RpcMessage::Request { jsonrpc: String::from("2.0"), id, method, params }
+        RpcMessage::Request {
+            jsonrpc: String::from("2.0"),
+            id,
+            method,
+            params,
+        }
     }
 
     pub fn response(id: RequestId, result: Option<Value>, error: Option<RpcError>) -> RpcMessage {
-        RpcMessage::Response { jsonrpc: String::from("2.0"), id, result, error }
+        RpcMessage::Response {
+            jsonrpc: String::from("2.0"),
+            id,
+            result,
+            error,
+        }
     }
 
     pub fn notification(method: String, params: Option<Value>) -> RpcMessage {
-        RpcMessage::Notification { jsonrpc: String::from("2.0"), method, params }
+        RpcMessage::Notification {
+            jsonrpc: String::from("2.0"),
+            method,
+            params,
+        }
     }
 }
